b14326b063e833f257e09908cb3ccf0d7a5ff7b2
Marcin Radoszewski <moriturius@gmail.com>
2025-11-02 12:45:04 +0100

    refactor native: replace type switches with Series interface in series_helpers.go

--- internal/native/series_helpers.go
+++ internal/native/series_helpers.go
@@ -7,172 +7,94 @@ import (
 )
 
 func seriesBack(series core.Value) (core.Value, error) {
-	var currentIndex int
-
-	switch s := series.(type) {
-	case *value.BlockValue:
-		currentIndex = s.GetIndex()
-	case *value.StringValue:
-		currentIndex = s.GetIndex()
-	case *value.BinaryValue:
-		currentIndex = s.GetIndex()
-	default:
+	seriesVal, ok := series.(value.Series)
+	if !ok {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDTypeMismatch, [3]string{"series", value.TypeToString(series.GetType()), ""})
 	}
 
+	currentIndex := seriesVal.GetIndex()
 	if currentIndex <= 0 {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDOutOfBounds, [3]string{"series is at head", "", ""})
 	}
 
-	var newSeries core.Value
-	switch s := series.(type) {
-	case *value.BlockValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BlockValue).SetIndex(currentIndex - 1)
-	case *value.StringValue:
-		newSeries = s.Clone()
-		newSeries.(*value.StringValue).SetIndex(currentIndex - 1)
-	case *value.BinaryValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BinaryValue).SetIndex(currentIndex - 1)
-	}
+	newSeries := seriesVal.Clone()
+	newSeries.SetIndex(currentIndex - 1)
 
-	return newSeries, nil
+	return newSeries.(core.Value), nil
 }
 
 func seriesNext(series core.Value) (core.Value, error) {
-	var currentIndex, length int
-
-	switch s := series.(type) {
-	case *value.BlockValue:
-		currentIndex = s.GetIndex()
-		length = len(s.Elements)
-	case *value.StringValue:
-		currentIndex = s.GetIndex()
-		length = len(s.String())
-	case *value.BinaryValue:
-		currentIndex = s.GetIndex()
-		length = len(s.Bytes())
-	default:
+	seriesVal, ok := series.(value.Series)
+	if !ok {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDTypeMismatch, [3]string{"series", value.TypeToString(series.GetType()), ""})
 	}
+
+	currentIndex := seriesVal.GetIndex()
+	length := seriesVal.Length()
 
 	newIndex := currentIndex + 1
 	if newIndex > length {
 		newIndex = length
 	}
 
-	var newSeries core.Value
-	switch s := series.(type) {
-	case *value.BlockValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BlockValue).SetIndex(newIndex)
-	case *value.StringValue:
-		newSeries = s.Clone()
-		newSeries.(*value.StringValue).SetIndex(newIndex)
-	case *value.BinaryValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BinaryValue).SetIndex(newIndex)
-	}
+	newSeries := seriesVal.Clone()
+	newSeries.SetIndex(newIndex)
 
-	return newSeries, nil
+	return newSeries.(core.Value), nil
 }
 
 func seriesHead(series core.Value) (core.Value, error) {
-	switch series.(type) {
-	case *value.BlockValue, *value.StringValue, *value.BinaryValue:
-		// Valid series types
-	default:
+	seriesVal, ok := series.(value.Series)
+	if !ok {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDTypeMismatch, [3]string{"series", value.TypeToString(series.GetType()), ""})
 	}
 
-	var newSeries core.Value
-	switch s := series.(type) {
-	case *value.BlockValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BlockValue).SetIndex(0)
-	case *value.StringValue:
-		newSeries = s.Clone()
-		newSeries.(*value.StringValue).SetIndex(0)
-	case *value.BinaryValue:
-		newSeries = s.Clone()
-		newSeries.(*value.BinaryValue).SetIndex(0)
-	}
+	newSeries := seriesVal.Clone()
+	newSeries.SetIndex(0)
 
-	return newSeries, nil
+	return newSeries.(core.Value), nil
 }
 
 func seriesIndex(series core.Value) (core.Value, error) {
-	var index int
-
-	switch s := series.(type) {
-	case *value.BlockValue:
-		index = s.GetIndex()
-	case *value.StringValue:
-		index = s.GetIndex()
-	case *value.BinaryValue:
-		index = s.GetIndex()
-	default:
+	seriesVal, ok := series.(value.Series)
+	if !ok {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDTypeMismatch, [3]string{"series", value.TypeToString(series.GetType()), ""})
 	}
 
+	index := seriesVal.GetIndex()
 	return value.NewIntVal(int64(index + 1)), nil
 }
 
 func seriesAt(series core.Value, index int) (core.Value, error) {
-	var length int
-	var element core.Value
-
-	switch s := series.(type) {
-	case *value.BlockValue:
-		length = len(s.Elements)
-		if index >= 0 && index < length {
-			element = s.Elements[index]
-		}
-	case *value.StringValue:
-		str := s.String()
-		length = len(str)
-		if index >= 0 && index < length {
-			element = value.NewStrVal(string(str[index]))
-		}
-	case *value.BinaryValue:
-		data := s.Bytes()
-		length = len(data)
-		if index >= 0 && index < length {
-			element = value.NewIntVal(int64(data[index]))
-		}
-	default:
+	seriesVal, ok := series.(value.Series)
+	if !ok {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDTypeMismatch, [3]string{"series", value.TypeToString(series.GetType()), ""})
 	}
 
+	length := seriesVal.Length()
 	if index < 0 || index >= length {
 		return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDIndexOutOfRange, [3]string{"at", "series", "index out of range"})
 	}
 
-	return element, nil
+	return seriesVal.ElementAt(index), nil
 }
 
 func seriesTail(series core.Value) (core.Value, error) {
-	var length int
-
 	switch s := series.(type) {
 	case *value.BlockValue:
-		length = len(s.Elements)
-		if length == 0 {
+		if len(s.Elements) == 0 {
 			return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDOutOfBounds, [3]string{"series is empty", "", ""})
 		}
 		return value.NewBlockVal(append([]core.Value{}, s.Elements[1:]...)), nil
 	case *value.StringValue:
 		str := s.String()
-		length = len(str)
-		if length == 0 {
+		if len(str) == 0 {
 			return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDOutOfBounds, [3]string{"series is empty", "", ""})
 		}
 		return value.NewStrVal(str[1:]), nil
 	case *value.BinaryValue:
 		data := s.Bytes()
-		length = len(data)
-		if length == 0 {
+		if len(data) == 0 {
 			return value.NewNoneVal(), verror.NewScriptError(verror.ErrIDOutOfBounds, [3]string{"series is empty", "", ""})
 		}
 		return value.NewBinaryVal(data[1:]), nil
