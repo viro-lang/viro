19a35f1047852e9b1e52f4e5d4d0f37c11f2f939
Marcin Radoszewski <moriturius@gmail.com>
2025-11-30 17:48:08 +0100

    test: add webui-navigate to availability test

--- test/contract/webui_availability_test.go
+++ test/contract/webui_availability_test.go
@@ -37,6 +37,7 @@ func TestWebUIAvailability(t *testing.T) {
 		{"webui-set-icon", "webui-set-icon"},
 		{"webui-send-raw", "webui-send-raw"},
 		{"webui-start-server", "webui-start-server"},
+		{"webui-navigate", "webui-navigate"},
 		{"webui-get-url", "webui-get-url"},
 	}
 
